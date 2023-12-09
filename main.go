b904d8b55a649e6a1bcc73e778ce9ab340f8092d
Sintario Satya <sintarios000@gmail.com>
2023-12-09 13:09:32 +0900

    first init

--- main.go
+++ main.go
@@ -6,11 +6,10 @@ import (
 	"net/http"
 )
 
-// PORT const is the const of the port :D
-const PORT = 9080 //ubdah port sesuai dengan NPM kalian masing - masing. contoh: jika npm 54422423 maka portnya 22423
+const PORT = 28080 //ubdah port sesuai dengan NPM kalian masing - masing. contoh: jika npm 54422423 maka portnya 22423
 
 func main() {
-	http.HandleFunc("/api/mahasiswa", user)                    // buat route untuk api/mahasiswa
+	http.HandleFunc("/api/mahasiswa", user) // buat route untuk api/mahasiswa
 	http.Handle("/", http.FileServer(http.Dir("polymer_npm"))) // buat route untuk file statis
 	fmt.Printf("web berjalan pada Port %d. url adalah http://[::1]:%d/", PORT, PORT)
 	fmt.Println()
@@ -18,43 +17,21 @@ func main() {
 }
 
 //TODO: buatlah struct lepkom dengan atribut Nama, Kursus, dan Foto
-type lepkom struct {
-	Nama   string `json:"nama_mahasiswa"`
-	Kursus string `json:"kursus_mahasiswa"`
-	Foto   string `json:"foto_mahasiswa"`
-}
 
 //TODO: buatlah slice lepkom dengan data 3 mahasiswa
-var dataMahasiswa = []lepkom{
-	{
-		Nama:   "SINTARIO SATYA",
-		Kursus: "Fundamental Web",
-		Foto:   "img/gambar1.jpg",
-	},
-	{
-		Nama:   "SINTARIO SATYA",
-		Kursus: "Golang for Beginner",
-		Foto:   "img/gambar2.png",
-	},
-	{
-		Nama:   "SINTARIO SATYA",
-		Kursus: "Golang for Intermediate",
-		Foto:   "img/gambar3.jpg",
-	},
-}
 
-func user(w http.ResponseWriter, r *http.Request) {
+func user(w http.ResponseWriter, r *http.Request) { 
 	w.Header().Set("Content-type", "application/json") // set header content-type menjadi json
 	if r.Method == http.MethodGet {
 
-		result, err := json.Marshal(dataMahasiswa) // ubah variabel dataMahasiswa menjadi json
+		result, err := json.Marshal(data_mahasiswa) // ubah variabel data_mahasiswa menjadi json
 
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError) // jika terjadi error, tampilkan error 500
 			return
 		}
 
-		w.Write(result) // tampilkan data json
+		w.Write(result)	// tampilkan data json
 		return
 	}
 }
